34ff564f39a6ef07acdab11a72d3fb2d852745ea
Summayya Hashmani <58825865+sumaiya2908@users.noreply.github.com>
2021-04-29 23:42:07 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,7 +43,6 @@ const App = () => {
             <Route path='/product/:id' component={ProductScreen} />
             <Route path='/cart/:id?' component={CartScreen} />
             <Route exact path='/search/:keyword' component={HomeScreen} />
-            <Route exact path="/category/:id" component={HomeScreen} />
             <Route exact path='/page/:pageNumber' component={HomeScreen} />
             <Route exact path='/search/:keyword/page/:pageNumber' component={HomeScreen} />
             <Route exact path='/' component={HomeScreen} />
